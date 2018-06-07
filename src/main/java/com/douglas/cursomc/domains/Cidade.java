9fdb6d98147b51df092960f55c1e65dd35b1dead
Douglas Nunes <douglasnunes.m@gmail.com>
2018-06-07 00:00:28 -0300

    Estado/Cidade

---
+++ src/main/java/com/douglas/cursomc/domains/Cidade.java
@@ -0,0 +1,73 @@
+package com.douglas.cursomc.domains;
+
+import java.io.Serializable;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Cidade implements Serializable{
+
+	private static final long serialVersionUID = 901538361708931078L;
+	
+	@Id
+	private Integer id;
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID_ESTADO")
+	private Estado estado = new Estado();
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+}
