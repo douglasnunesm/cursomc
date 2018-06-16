7b606330f9dfc086fe80814d6fee8d905c3bed4c
Douglas Nunes <douglasnunes.m@gmail.com>
2018-06-16 19:15:19 -0300

    Autorização Implementação

--- src/main/java/com/douglas/cursomc/config/SecurityConfig.java
+++ src/main/java/com/douglas/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.douglas.cursomc.security.JWTAuthenticationFilter;
+import com.douglas.cursomc.security.JWTAuthorizationFilter;
 import com.douglas.cursomc.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
